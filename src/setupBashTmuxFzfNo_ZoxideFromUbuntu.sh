b17cd6f37e59cce2f52fda642f489ae9cd2eccc1
Tuan ITF Dev <tuanitfdev@gmail.com>
2026-04-02 19:27:47 +0700

    Add new case no zoxide

---
+++ src/setupBashTmuxFzfNo_ZoxideFromUbuntu.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+apt update -y && apt install -y curl vim-gtk3 tmux xsel htop net-tools iputils-ping 
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone https://github.com/lincheney/fzf-tab-completion.git ~/fzf-tab-completion
+cp ~/.bashrc ~/.bashrc.bk
+cp script/.bashrcNoZoxide ~/.bashrc
+cp conf/.vimrc ~/.vimrc
+cp conf/.tmux.conf ~/.tmux.conf
+vim +PlugInstall +qall
+~/.tmux/plugins/tpm/bin/install_plugins
