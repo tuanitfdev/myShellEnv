ff46a75c30a741e4544f4d4147417af6acd78295
Tuan ITF Dev <tuanitfdev@gmail.com>
2026-04-02 16:38:25 +0700

    init script project

---
+++ src/setupBashTmuxFzfZoxideFromUbuntu.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+apt update -y && apt install -y curl vim-gtk3 tmux xsel htop net-tools iputils-ping 
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
+git clone https://github.com/lincheney/fzf-tab-completion.git ~/fzf-tab-completion
+cp ~/.bashrc ~/.bashrc.bk
+cp script/.bashrc ~/.bashrc
+cp conf/.vimrc ~/.vimrc
+cp conf/.tmux.conf ~/.tmux.conf
+vim +PlugInstall +qall
+~/.tmux/plugins/tpm/bin/install_plugins
