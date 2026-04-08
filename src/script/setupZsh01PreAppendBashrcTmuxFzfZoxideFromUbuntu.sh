043b07de9f952cd6fddadb7d44497452999ada1d
Tuan ITF Dev <tuanitfdev@gmail.com>
2026-04-08 18:54:39 +0700

    update to check issue

--- src/script/setupZsh01PreAppendBashrcTmuxFzfZoxideFromUbuntu.sh
+++ src/script/setupZsh01PreAppendBashrcTmuxFzfZoxideFromUbuntu.sh
@@ -24,5 +24,5 @@ cp conf/.vimrc ~/.vimrc
 cp conf/.tmux.conf ~/.tmux.conf
 vim +PlugInstall +qall
 ~/.tmux/plugins/tpm/bin/install_plugins
-TERM=xterm && zsh -i -c "exit"
+export TERM=xterm && zsh -i -c "exit"
 
