b7e140f2ef670b1bc1ca724d7827a43f876b56c8
Tuan ITF Dev <tuanitfdev@gmail.com>
2026-07-05 12:19:29 +0700

    build proxy/vpn server script

--- src/script/setupDante.sh
+++ src/script/setupDante.sh
@@ -5,5 +5,5 @@ systemctl stop danted.service
 useradd -r -s /bin/false tuanitfs
 echo 'tuanitfs:1tfTn&Sks' | chpasswd
 mv /etc/danted.conf /etc/danted.conf.org
-cp conf/danted.conf /etc/squid/danted.conf
-systemctl restart danted
+cp conf/danted.conf /etc/danted.conf
+# systemctl restart danted
--- src/setupProxyVpnServer.sh
+++ src/setupProxyVpnServer.sh
@@ -5,3 +5,9 @@ bash ./script/setupSquid.sh
 bash ./script/installDocker.sh
 bash ./script/setupWireguard.sh
 chsh -s $(which zsh)
+ufw allow 41080
+ufw allow 31288
+ufw allow 31821
+ufw allow 31820/udp
+ufw enable
+ufw reload
